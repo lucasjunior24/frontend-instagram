e58d86413fc8e0a23ddd04c33ceebf3504499d2d
lucasjunior24 <lukateku@gmail.com>
2021-03-06 18:18:11 -0300

    estilizando footer

--- src/Components/BottonNav.js
+++ src/Components/BottonNav.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { MdHome, MdPhotoCamera, MdPerson } from 'react-icons/md'
+import { Link } from 'react-router-dom'
 
 function BottonNav() {
     return (
         <footer>
             <nav>
-                <MdHome />
-                <MdPhotoCamera />
-                <MdPerson />
-                
+                <Link to='/feed'>
+                    <MdHome seze={20} />
+                </Link>
+                <Link to='/post'>
+                    <MdPhotoCamera seze={20} />
+                </Link>
+                <Link to='/profile'>
+                    <MdPerson seze={20} />
+                </Link>
             </nav>
         </footer>
     )
