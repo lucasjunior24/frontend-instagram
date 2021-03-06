0acd4c3058bf0b8f3bb6b07da814c8ee69a37928
lucasjunior24 <lukateku@gmail.com>
2021-03-06 19:13:28 -0300

    personalizando placeholder do input

--- src/Components/InputStandart.js
+++ src/Components/InputStandart.js
@@ -4,7 +4,7 @@ function InputStandart(props) {
     return (
         <div className='input-field'>
             <label htmlFor='user'>{props.title}</label>
-            <input type='text' id='user' placeholder='Usuário' />
+            <input type='text' id='user' placeholder={`Preencha o campo ${props.title.toLowerCase()} `} />
         </div>
     )
 }
