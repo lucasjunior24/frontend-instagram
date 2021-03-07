6c4cd224179cf417374f64a44ca7ed78601dd723
lucasjunior24 <lukateku@gmail.com>
2021-03-07 16:21:20 -0300

    Criando card da  pagina do feed

---
+++ src/Components/FeedCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeedCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default FeedCard
--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -6,7 +6,24 @@ function Feed() {
     return (
         <>
             <HeaderNav />
-            <h1>Feed</h1>
+            <div className='card-container'>
+                <div className='card-header'>
+                    <img src='' alt='Foto de perfil' />
+                    <h2>Perfil do Usuário</h2>
+                </div>
+                <div className='card-photo'>
+                    <img src='' alt='imagem' />
+                </div>
+                <div className='card-footer'>
+                    <div className='card-metadata'>
+                        <img src='' alt='Likes' />
+                        <h3>43 pessoas gostaram</h3>
+                    </div>
+                    <div className='card-info'>
+                        <p><a href='/feed' >Nome do Usuário</a> Está é adescrição</p>
+                    </div>
+                </div>
+            </div>
             <BottonNav />
         </>
     )
