d2eccdabf60ad8bad531cb3f415adb0da5698ef2
lucasjunior24 <lukateku@gmail.com>
2021-03-07 15:42:30 -0300

    Criando estado para carregamento da pagina

--- src/Components/RegisterMainSection.js
+++ src/Components/RegisterMainSection.js
@@ -14,6 +14,8 @@ function RegisterMainSection() {
     const [site, setSite] = useState('')
     const [avatar, setAvatar] = useState('')
 
+    const [loading, setLoading] = useState(false)
+
     function loadFistPage(e) {
         e.preventDefault()
         setShow(1)
@@ -24,11 +26,13 @@ function RegisterMainSection() {
     }
     async function registrationHandle(e) {
         e.preventDefault()
+        setLoading(true)
         try { 
             await api.post('users', {
                 username, password, name, description, site, avatar
             })
             alert('Usuário cadastrado com sucesso!')
+            setLoading(false)
             setUsername('')
             setPassword('')
             setName('')
@@ -37,6 +41,7 @@ function RegisterMainSection() {
             setAvatar('')
          } catch(err) {
             alert('Erro ao tentar cadastrar o usuário, tente novamente|')
+            setLoading(false)
         }
     }
     return (
@@ -44,65 +49,72 @@ function RegisterMainSection() {
             <div className='form-login-register'>
                 <img alt='Logo do Instagram' src={HeroImg}/>
                 <form>
-                    <h1>Cadastrar-se</h1>
-                    {show  === 1?
-                    <fieldset>
-                        <InputStandart 
-                            state={username} 
-                            setState={e => setUsername(e.target.value)} 
-                            type='text' 
-                            typeId='username' 
-                            title='Usuário'/>
-                        <InputStandart 
-                            state={password} 
-                            setState={e => setPassword(e.target.value)}
-                            type='password' 
-                            typeId='password' 
-                            title='Senha' />
-                        <InputStandart 
-                            state={name} 
-                            setState={e => setName(e.target.value)}
-                            type='text' 
-                            typeId='name' 
-                            title='Nome' />
-                    </fieldset> :
-                    <fieldset>
-                        <InputStandart 
-                            state={description} 
-                            setState={e => setDescription(e.target.value)}
-                            type='text' 
-                            typeId='Descrição' 
-                            title='Descrição'/>
-                        <InputStandart 
-                            state={site} 
-                            setState={e => setSite(e.target.value)}
-                            type='text' 
-                            typeId='Site' 
-                            title='Site' />
-                        <InputStandart 
-                            state={avatar} 
-                            setState={e => setAvatar(e.target.value)}
-                            type='text' 
-                            typeId='Avatar' 
-                            title='Avatar' />
-                    </fieldset> }
-                    <div className='form-navigation'>
-                        {show === 1 ?
-                            <button 
-                                style={{ background: '#0095f6' }}
-                                onClick={loadFistPage}>1</button> :
-                            <button onClick={loadFistPage}>1</button>}
-                        {show === 2 ?
-                            <button
-                            style={{ background: '#0095f6' }} 
-                                onClick={loadSecondPage}>2</button> :
-                            <button onClick={loadSecondPage}>2</button>}
-                         {avatar && site && description && name && password && username &&
-                            <button 
-                                className='submit'
-                                onClick={registrationHandle}
-                            >Finalizar</button>}
-                    </div>
+                    {loading ? 
+                        <h1>Criando usuário...</h1>
+                    :
+                        <>
+                            <h1>Cadastrar-se</h1>
+                            {show  === 1?
+                            <fieldset>
+                                <InputStandart 
+                                    state={username} 
+                                    setState={e => setUsername(e.target.value)} 
+                                    type='text' 
+                                    typeId='username' 
+                                    title='Usuário'/>
+                                <InputStandart 
+                                    state={password} 
+                                    setState={e => setPassword(e.target.value)}
+                                    type='password' 
+                                    typeId='password' 
+                                    title='Senha' />
+                                <InputStandart 
+                                    state={name} 
+                                    setState={e => setName(e.target.value)}
+                                    type='text' 
+                                    typeId='name' 
+                                    title='Nome' />
+                            </fieldset> :
+                            <fieldset>
+                                <InputStandart 
+                                    state={description} 
+                                    setState={e => setDescription(e.target.value)}
+                                    type='text' 
+                                    typeId='Descrição' 
+                                    title='Descrição'/>
+                                <InputStandart 
+                                    state={site} 
+                                    setState={e => setSite(e.target.value)}
+                                    type='text' 
+                                    typeId='Site' 
+                                    title='Site' />
+                                <InputStandart 
+                                    state={avatar} 
+                                    setState={e => setAvatar(e.target.value)}
+                                    type='text' 
+                                    typeId='Avatar' 
+                                    title='Avatar' />
+                            </fieldset> }
+                            <div className='form-navigation'>
+                                {show === 1 ?
+                                    <button 
+                                        style={{ background: '#0095f6' }}
+                                        onClick={loadFistPage}>1</button> :
+                                    <button onClick={loadFistPage}>1</button>}
+                                {show === 2 ?
+                                    <button
+                                    style={{ background: '#0095f6' }} 
+                                        onClick={loadSecondPage}>2</button> :
+                                    <button onClick={loadSecondPage}>2</button>}
+                                {avatar && site && description && name && password && username &&
+                                    <button 
+                                        className='submit'
+                                        onClick={registrationHandle}
+                                    >Finalizar</button>}
+                            </div>
+                        </>
+                    }
+                    
                 </form>
             </div>
         </main>
