65abba339926a7434e255b77b5b3c23b75ba01e9
lucasjunior24 <lukateku@gmail.com>
2021-03-14 17:22:17 -0300

    Mostrar likes

--- src/Components/FeedCard.js
+++ src/Components/FeedCard.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import FillHeart from '../Assets/heart-red.svg'
 import OutHeart from '../Assets/heart.svg'
 
 function FeedCard({ picture, description, likes, user }) {
+    const [userId, setUserID] = useState(localStorage.getItem('InstagramUserId'))
+
     return (
         <div className='card-container'>
             <div className='card-header'>
@@ -19,7 +21,11 @@ function FeedCard({ picture, description, likes, user }) {
             </div>
             <div className='card-footer'>
                 <div className='card-metadata'>
-                    <img src={OutHeart} alt='Likes' />
+                    {likes.includes(userId) ? 
+                        <img src={FillHeart} alt='Likes' /> : 
+                        <img src={OutHeart} alt='Likes' />
+                    }
+                    
                     {likes.length > 1 ? 
                         <h3>{likes.length} pessoas gostaram</h3> : likes.length === 1 ?
                         <h3>1 pessoa gostou</h3> : null  
