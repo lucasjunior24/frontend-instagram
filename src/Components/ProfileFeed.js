89e975b14e7a8a05c4cf7d8b7017a9787fbc65a0
lucasjunior24 <lukateku@gmail.com>
2021-03-15 16:42:13 -0300

    Tornando o feed da pagina do perfil responsivo

--- src/Components/ProfileFeed.js
+++ src/Components/ProfileFeed.js
@@ -9,8 +9,21 @@ function ProfileFeed() {
                 
                 <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
             </div>
+            <div className='profile-pic'> 
+                <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
+            </div>
+            <div className='profile-pic'>
+                {/* <div className='profile-details'>
+                    < AiFillHeart />
+                    <p>50 Likes</p>
+                </div> */}
+                <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
+            </div>
             <div className='profile-pic'>
                 
+                <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
+            </div>
+            <div className='profile-pic'> 
                 <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
             </div>
             <div className='profile-pic'>
