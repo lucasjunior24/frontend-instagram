26c4063b9f2ddc1981884ae975e7fe8a3fba1cf2
lucasjunior24 <lukateku@gmail.com>
2021-03-15 15:14:55 -0300

    estilizando feed da pagina do perfil

--- src/Components/ProfileFeed.js
+++ src/Components/ProfileFeed.js
@@ -5,21 +5,21 @@ import { AiFillHeart } from 'react-icons/ai'
 function ProfileFeed() {
     return (
         <div className='profile-pics'>
-            <div>
+            <div className='profil-pic'>
                 <div className='profile-details'>
                     < AiFillHeart />
                     <p>50 Likes</p>
                     <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
                 </div>
             </div>
-            <div>
+            <div className='profil-pic'>
                 <div className='profile-details'>
                     < AiFillHeart />
                     <p>50 Likes</p>
                 </div>
                 <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
             </div>
-            <div>
+            <div className='profil-pic'>
                 <div className='profile-details'>
                     < AiFillHeart />
                     <p>50 Likes</p>
