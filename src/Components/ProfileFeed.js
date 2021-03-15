a2225343413c983c432b70bbeee3201efc1f40a6
lucasjunior24 <lukateku@gmail.com>
2021-03-15 15:27:54 -0300

    Adptando feed da pagina do perfil

--- src/Components/ProfileFeed.js
+++ src/Components/ProfileFeed.js
@@ -5,27 +5,22 @@ import { AiFillHeart } from 'react-icons/ai'
 function ProfileFeed() {
     return (
         <div className='profile-pics'>
-            <div className='profil-pic'>
-                <div className='profile-details'>
-                    < AiFillHeart />
-                    <p>50 Likes</p>
-                    <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
-                </div>
-            </div>
-            <div className='profil-pic'>
-                <div className='profile-details'>
-                    < AiFillHeart />
-                    <p>50 Likes</p>
-                </div>
+            <div className='profile-pic'>
+                
                 <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
             </div>
-            <div className='profil-pic'>
-                <div className='profile-details'>
+            <div className='profile-pic'>
+                
+                <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
+            </div>
+            <div className='profile-pic'>
+                {/* <div className='profile-details'>
                     < AiFillHeart />
                     <p>50 Likes</p>
-                </div>
+                </div> */}
+                <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
             </div>
-            <img src='https://th.bing.com/th/id/OIP.eJHIsU7S8Tx0nDfKQp4Z0AHaEK?w=280&h=180&c=7&o=5&pid=1.7' alt='avatar' />
+           
 
         </div>
     )
