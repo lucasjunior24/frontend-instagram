b93b0ee10acec28ab33b0d1bdd9e0ddac743c8bf
lucasjunior24 <lukateku@gmail.com>
2021-03-16 18:13:17 -0300

    Obtendo dados do usuario

--- src/Components/ProfileContainer.js
+++ src/Components/ProfileContainer.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import api from '../Services/api'
+
 import { AiFillHeart } from 'react-icons/ai'
+
 import ProfileFeed from './ProfileFeed'
 import ProfileHeader from './ProfileHeader'
 
 function ProfileContainer() {
+    const [userId] = useState(localStorage.getItem('InstagramUserId'))
+    const [userInformations, setUserInformations] = useState({})
+    
+    useEffect(() => {
+        async function getProfileInfo() {
+            console.log('oi')
+            try {
+                const profileInfo = await api.get(`users/${userId}`)
+                const { data } = profileInfo
+                const userInfo = data.userInfo  
+                setUserInformations(userInfo)
+            } catch(err) {
+                alert('Erro ao carregar os dados')
+            }
+        }
+        getProfileInfo()
+    }, [])
+
     return (
         <main className='profile-container'>
-            <ProfileHeader />
+            <ProfileHeader
+                username={userInformations.username}
+                name={userInformations.name}
+                description={userInformations.description}
+                site={userInformations.site}
+                avatar={userInformations.avatar}
+            />
             <ProfileFeed />
         </main>
     )
--- src/Components/ProfileHeader.js
+++ src/Components/ProfileHeader.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-function ProfileHeader() {
+function ProfileHeader({username, name, description, site, avatar }) {
     return (
         <div className='profile-header'>
-            <img className='avatar' src='' alt='avatar' />
+            <img className='avatar' src={avatar} alt='avatar' />
             <div className='profile-user-info'>
-                <h1>USERNAME</h1>
-                <h2>Nome do usuairo</h2>
-                <p>Descricao do usuario</p>
-                <a href=''>Site</a>
+                <h1>{username}</h1>
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <a href=''>{site}</a>
             </div>
         </div>
     )
