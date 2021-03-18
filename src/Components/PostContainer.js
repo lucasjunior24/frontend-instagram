3e7f088cd32b5d69d3dfb0b50b4d006d276612a8
lucasjunior24 <lukateku@gmail.com>
2021-03-18 12:41:17 -0300

    Estilizando imagem  da pagina de Posts

--- src/Components/PostContainer.js
+++ src/Components/PostContainer.js
@@ -3,8 +3,8 @@ import InputStandart from '../Components/InputStandart'
 
 function PostContainer() {
     return (
-        <div className='post-container'>
-            <img src='' alt='' />
+        <main className='post-container'>
+            <img src='https://th.bing.com/th/id/OIP.EZrn_SyHP3i578RpQLWXSQHaFj?pid=ImgDet&rs=1' alt='' />
             <div>
                 <InputStandart 
                     title='Usuario'
@@ -14,7 +14,7 @@ function PostContainer() {
                 />
                 <button>Postar Imagem</button>
             </div>
-        </div>
+        </main>
     )
 }
 
--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import BottonNav from '../Components/BottonNav'
 import HeaderNav from '../Components/HeaderNav'
 import PostContainer from '../Components/PostContainer'
